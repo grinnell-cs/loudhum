236b173cb5b21120f1fc1b8b8ae9d46abb6365e2
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2018-11-12 10:20:05 -0600

    Add short package description.

--- info.rkt
+++ info.rkt
@@ -3,6 +3,6 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/loudhum.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
+(define pkg-desc "Racket code for the FunDHum textbook")
+(define version "0.1")
 (define pkg-authors '(rebelsky))
